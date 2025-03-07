531c77b4905567c53a993bfd395da4b57ac2b25e
harish-jai <harish.jaisankar@gmail.com>
2025-03-06 20:36:55 -0500

    debug statements

--- display/sentiment-frontend/src/App.tsx
+++ display/sentiment-frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
 
-const API_BASE = import.meta.env.REACT_APP_API_BASE || "http://localhost:5000";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
 console.log(API_BASE);
 
 interface Ticker {
